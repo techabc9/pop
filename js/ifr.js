150926dbae5922f3f4ba548bfdef67c415338574
techabc9 <chopildoo@daum.net>
2014-03-21 17:45:13 +0900

    pop2

--- js/ifr.js
+++ js/ifr.js
@@ -1,7 +1,28 @@
 // JavaScript Document
+var _ly_slider;
 
 $(document).ready(function(){
 	$('#slider').bxSlider({
-		pager : false
+		controls : false,
+		mode : 'fade',
+		auto : true
+	});
+	
+	$("#ly_close").click(function(e) {
+		$("#layer").stop(true,true).animate({left:1200}, function(){
+			_ly_slider.destroySlider();
+		});
+		e.preventDefault();
 	});
 });
+
+function set_slide(n){
+	_ly_slider = $('#ly_slider').bxSlider({
+		startSlide: n,
+		pager : false,
+		infiniteLoop : false,
+		onSliderLoad: function(currentIndex){
+			$("#layer").stop(true,true).animate({left:0});
+		}
+	});
+}
