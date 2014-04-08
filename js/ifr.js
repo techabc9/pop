9a5abb36928ff0f15e38dfdb1b10c261e16e8a5c
techabc9 <chopildoo@daum.net>
2014-04-08 16:19:28 +0900

    pop6

--- js/ifr.js
+++ js/ifr.js
@@ -14,6 +14,16 @@ $(document).ready(function(){
 		});
 		e.preventDefault();
 	});
+	
+	$('#faqList > li').on('click', function(e) {
+		if(e.type === 'click' && $(this).hasClass('on') == false){
+			$('#faqList > li').removeClass('on');
+			$('#faqList > li').find('dd').slideUp();
+			$(this).addClass('on');
+			$(this).find('dd').slideDown();
+		}
+	});
+	
 });
 
 function set_slide(n){
